496832759134249eed6ad9a61d99060af8074d42
benjaminrodriguez <taff@MacBook-Air-de-Adriana.local>
2015-01-18 19:36:48 -0600

    Initial Commit

---
+++ Lab01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lab01
+//
+//  Created by Taff on 18/01/15.
+//  Copyright (c) 2015 Taff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lab01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab01
+//
+//  Created by Taff on 18/01/15.
+//  Copyright (c) 2015 Taff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
