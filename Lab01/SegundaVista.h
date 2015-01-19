14d049fba4c4b4bc3ed767f9fa5dad456bc37d44
benjaminrodriguez <taff@MacBook-Air-de-Adriana.local>
2015-01-18 21:39:32 -0600

    Primera parte

---
+++ Lab01/SegundaVista.h
@@ -0,0 +1,18 @@
+//
+//  SegundaVista.h
+//  Lab01
+//
+//  Created by Taff on 18/01/15.
+//  Copyright (c) 2015 Taff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegundaVista : UIViewController
+@property (strong, nonatomic) IBOutlet UIImageView *imgSegunda;
+
+@property (strong, nonatomic) IBOutlet UILabel *btnNom2;
+@property (strong, nonatomic) IBOutlet UILabel *btnEdad2;
+
+
+@end
---
+++ Lab01/SegundaVista.m
@@ -0,0 +1,37 @@
+//
+//  SegundaVista.m
+//  Lab01
+//
+//  Created by Taff on 18/01/15.
+//  Copyright (c) 2015 Taff. All rights reserved.
+//
+
+#import "SegundaVista.h"
+
+@interface SegundaVista ()
+
+@end
+
+@implementation SegundaVista
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
